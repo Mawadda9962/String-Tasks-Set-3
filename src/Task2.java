4bb0e8829ffecc148361e2b23d618ff2a4f0f41d
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-21 15:41:46 +0400

    Solving Task2 part6

--- src/Task2.java
+++ src/Task2.java
@@ -1,7 +1,7 @@
 public class Task2 {
     public static void mian(String[] args){
 
-        String word = {Hello there", "llo"};
+        String word = Hello there", "llo"};
 
 
         System.out.println(word);
