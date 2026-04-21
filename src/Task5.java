eec00ba6767534724d8632717934d24c652b844d
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-21 16:47:20 +0400

    Solving Task5 part1

---
+++ src/Task5.java
@@ -0,0 +1,14 @@
+public class Task5 {
+    public static void main(String[] args){
+
+
+    }
+
+    public static Integer appearingThreeTimes(String str){
+
+        int count = 0;
+
+        for()
+    }
+
+}
