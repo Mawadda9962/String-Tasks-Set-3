9f9afbb00290a37f692ad95780b990c00481487e
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-21 16:09:27 +0400

    Solving Task3 part3

--- src/Task3.java
+++ src/Task3.java
@@ -6,6 +6,22 @@ public class Task3 {
     }
     public static boolean isEqualToNot (String str){
 
+        int countIs = 0;
+        int countNot = 0;
+
+        for(int i = 0; i < str.length(); i++){
+            if (str.substring(i, i + 2).equals("is")){
+                countIs++;
+
+            }
+        }
+
+        for (int i = 0; i < str.length(); i++){
+            if (str.substring(i, i + 3).equals("Not")){
+                countNot++;
+            }
+        }
+
 
     }
 }
