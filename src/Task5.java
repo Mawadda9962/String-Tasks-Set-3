5d0e45820c034a92383748772d3329010135e8e9
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-21 16:56:58 +0400

    Solving Task5 appearing Three Times

--- src/Task5.java
+++ src/Task5.java
@@ -3,16 +3,17 @@ public class Task5 {
 
 
         System.out.println(appearingThreeTimes("abcXXXabc"));
+        System.out.println(appearingThreeTimes("xxxabyyyycd"));
     }
 
     public static Integer appearingThreeTimes(String str){
 
         int count = 0;
 
-        for(int i = 0; i < str.length(); i++){
+        for(int i = 0; i < str.length() -2; i++){
 
             if(str.charAt(i) == str.charAt(i + 1) && str.charAt(i) == str.charAt(i + 2)){
-                return count++;
+                 count++;
             }
         }
         return count;
