8aa27e453313e8160ee3df55319eee3c6997c310
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-21 16:55:02 +0400

    Solving Task5 part11

--- src/Task5.java
+++ src/Task5.java
@@ -2,7 +2,7 @@ public class Task5 {
     public static void main(String[] args){
 
 
-        System.out.println();
+        System.out.println(appearingThreeTimes("abcXXXabc"));
     }
 
     public static Integer appearingThreeTimes(String str){
