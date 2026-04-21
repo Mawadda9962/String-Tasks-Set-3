af0bd1343875a2c4a9959b3f7c79153a1f4281d4
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-21 16:00:33 +0400

    Solving Task3 part1

--- src/Task2.java
+++ src/Task2.java
@@ -1,23 +1,19 @@
 public class Task2 {
-    public static void mian(String[] args){
+
+    public static void main(String[] args) {
 
         String base = "Hello there";
-        String remove =  "llo";
-
+        String remove = "llo";
 
         System.out.println(baseAndRemove(base, remove));
-
     }
-    public static String baseAndRemove(String base, String remove){
 
-        String result =" ";
+    public static String baseAndRemove(String base, String remove) {
 
-        for(int i = 0; i < base.length(); i++){
-            if(base.contains(remove)){
-                result = base.replace(remove, "");
-
-            }
+        if (base.contains(remove)) {
+            base = base.replace(remove, "");
         }
-        return result;
+
+        return base;
     }
 }
