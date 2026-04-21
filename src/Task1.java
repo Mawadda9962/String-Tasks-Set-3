7657b28bc0326a3895ba73c31e42a8ede8ac902c
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-21 14:10:18 +0400

    Solving Task1 part1

---
+++ src/Task1.java
@@ -0,0 +1,12 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Task1 {
+    public static void main(String[] args){
+
+        Set<String> employeeIds = new HashSet<>();
+
+
+
+    }
+}
