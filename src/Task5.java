61732e0df7e6bd3c8e0ad5de78592c3127f26f71
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-21 16:51:15 +0400

    Solving Task5 part7

--- src/Task5.java
+++ src/Task5.java
@@ -11,7 +11,7 @@ public class Task5 {
         for(int i = 0; i < str.length(); i++){
 
             if(str.charAt(i) == str.charAt(i + 1) && str.charAt(i) == str.charAt(i + 2)){
-                return
+                return count++;
             }
         }
     }
