6b53470279baa0c11f042daee0ca53a194a37d97
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-21 15:04:41 +0400

    Solving Task1 count Y and Z

--- src/Task1.java
+++ src/Task1.java
@@ -2,34 +2,31 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Task1 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         String word = "fez day";
         System.out.println(countYZ(word));
+    }
 
+    public static int countYZ(String str) {
+        Set<String> endingInYOrZ = new HashSet<>();
 
-        }
-        public static int countYZ(String str){
-        Set <String> endingInYOrZ = new HashSet<>();
-
-
-        endingInYOrZ.add("Y");
+        endingInYOrZ.add("y");
         endingInYOrZ.add("z");
 
         int count = 0;
+        str = str.toLowerCase();
 
-        for(int i = 0; i < str.length(); i++ ){
+        for (int i = 0; i < str.length(); i++) {
             String currentChar = String.valueOf(str.charAt(i));
 
-            if(endingInYOrZ.contains(currentChar)){
-                if(i == str.length() -1){
+            if (endingInYOrZ.contains(currentChar)) {
+                if (i == str.length() - 1 || !Character.isLetter(str.charAt(i + 1))) {
                     count++;
                 }
-
             }
-
-            return count;
-
         }
 
+        return count;
+    }
 }
