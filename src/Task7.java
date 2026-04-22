a561335143752ac131f5fd95482d0c5faab8d1a3
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-22 10:04:19 +0400

    Solving Task7 sameEnds

--- src/Task7.java
+++ src/Task7.java
@@ -1,15 +1,15 @@
 public class Task7 {
     public static void main(String[] args){
-        System.out.println("abXYab");
+        System.out.println(sameEnds("abXYab"));
 
     }
     public static String sameEnds (String str){
         String result = " ";
 
-        for(int i = 0; i < str.length(); i++){
+        for(int i = 0; i <= str.length() /2 ; i++){
 
             String first = str.substring(0,i);
-            String last = str.substring(str.length()-1);
+            String last = str.substring(str.length() - i);
 
             if (first.equals(last)){
                 result = first;
