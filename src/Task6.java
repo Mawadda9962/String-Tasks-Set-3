3219edf5a6db31bcb006006a3157c1d7c673a9fd
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-22 08:59:56 +0400

    Solving Task6 part2

--- src/Task6.java
+++ src/Task6.java
@@ -7,8 +7,10 @@ public class Task6 {
     public static Integer digitsFrom1To9 (String str){
 
         int sum = 0;
+        int num =
 
         for(int i = 0; i < str.length(); i++){
+            if(str.)
 
 
         }
