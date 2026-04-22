c550e7c7d841f9eb359327d590a2857788842abb
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-22 13:28:36 +0400

    Solving task11 adding class name

--- src/notReplace.java
+++ src/notReplace.java
@@ -1,20 +1,25 @@
 public class notReplace {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
 
     }
-    public static String notReplace (String str){
+
+    public static String notReplace(String str) {
         String result = "";
-        for(int i = 0; i < str.length(); i++){
-            if(i-1 >= 0 && Character.isLetter(str.charAt(i-1))
-                || i+2 < str.length() && Character.isLetter(str.charAt(i+2))){
+        for (int i = 0; i < str.length(); i++) {
+            if (i - 1 >= 0 && Character.isLetter(str.charAt(i - 1))
+                    || i + 2 < str.length() && Character.isLetter(str.charAt(i + 2))) {
 
                 result += str.charAt(i);
-            }
-            else if(i+1 < str.length() && str.substring(i, i+2).equals("is")){
+            } else if (i + 1 < str.length() && str.substring(i, i + 2).equals("is")) {
                 result += "is not";
+                i++;
 
-            }
+            } else result += str.charAt(i);
+
         }
+        return result;
+
     }
 }
+
