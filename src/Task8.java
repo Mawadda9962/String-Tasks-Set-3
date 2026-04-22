929e880138113fdd5699295913a681c4d9bd99e1
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-22 10:25:02 +0400

    Solving Task8

--- src/Task8.java
+++ src/Task8.java
@@ -11,11 +11,11 @@ public class Task8 {
             String first = str.substring(i, i+1);
             String last = str.substring(str.length() -1 - i, str.length()-i);
 
-            if (first == last){
-               result == first;
+            if (first.equals(last)){
+               result = first;
             }
         }
 
-        return result
+        return result;
     }
 }
