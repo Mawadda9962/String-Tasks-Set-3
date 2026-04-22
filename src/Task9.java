0be7794b3f2bb400511c33ab4f8d1a8bdde043d1
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-22 10:53:38 +0400

    Solving Task9

--- src/Task9.java
+++ src/Task9.java
@@ -1,7 +1,7 @@
 public class Task9 {
     public static void main(String[] args){
 
-        System.out.println(maxBlock());
+        System.out.println(maxBlock(""));
 
     }
     public static Integer maxBlock(String str){
