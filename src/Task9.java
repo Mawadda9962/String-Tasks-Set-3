2afbebc5f5b66b82d4b216573ed4759310559165
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-22 10:51:49 +0400

    Solving Task9

--- src/Task9.java
+++ src/Task9.java
@@ -4,6 +4,11 @@ public class Task9 {
     }
     public static Integer maxBlock(String str){
 
-        for(int i = 0; i < str.length(); )
+        int count = 0;
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == str.charAt(i+1)){
+                count++;
+            }
+        }
     }
 }
