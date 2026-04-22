9f50962a64fa948ed3832ad5c7c71dc5900c8bb7
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-22 09:32:31 +0400

    Solving Task7

--- src/Task7.java
+++ src/Task7.java
@@ -2,6 +2,9 @@ public class Task7 {
     public static void main(String[] args){
 
     }
-    public static
+    public static String sameEnds (String str){
+        int first = str.substring(0,1);
+        int last =
+    }
 
 }
