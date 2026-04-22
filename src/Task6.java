9ec98403da42eadd3e077da27411bd85cd0203a9
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-22 08:55:44 +0400

    Solving Task6 part1

---
+++ src/Task6.java
@@ -0,0 +1,16 @@
+public class Task6 {
+    public static void main(String[] args){
+
+
+
+    }
+    public static Integer digitsFrom1To9 (String str){
+
+        int sum = 0;
+
+        for(int i = 0; i < str.length(); i++){
+
+
+        }
+    }
+}
