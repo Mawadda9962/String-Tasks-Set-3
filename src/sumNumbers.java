d346cee99ea4ea51000ea4fa50d0425e731b3488
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-22 13:17:46 +0400

    Solving task10 sum Numbers

--- src/sumNumbers.java
+++ src/sumNumbers.java
@@ -7,9 +7,9 @@ public class sumNumbers {
         int sum = 0;
         String tem = "";
         for(int i =0; i < str.length(); i++){
-            if(Character.isDigit(str.charAt(i))){
+            if(Character.isDigit(str.charAt(i))) {
                 tem += str.charAt(i);
-                if(i == str.length()-1 || !Character.isDigit(str.charAt(i))){
+                if(i == str.length()-1 || !Character.isDigit(str.charAt(i+1))){
                     sum += Integer.parseInt(tem);
                     tem = "";
                 }
