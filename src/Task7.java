f1d00d1988c42d0997479316591473efc2734986
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-22 09:41:14 +0400

    Solving Task7

--- src/Task7.java
+++ src/Task7.java
@@ -4,12 +4,18 @@ public class Task7 {
     }
     public static String sameEnds (String str){
 
+        String result = " ";
         for(int i = 0; i < str.length(); i++){
 
             String first = str.substring(0,i);
             String last = str.substring(str.length()-1);
 
+            if (first.equals(last)){
+                result = first;
+            }
+
         }
+        return re
     }
 
 }
