59756a9ef3d91784aa6281470844e1204554feb6
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-22 10:34:09 +0400

    Solving Task8 mirror Ends

--- src/Task8.java
+++ src/Task8.java
@@ -2,12 +2,12 @@ public class Task8 {
     public static void main(String[] args){
 
         System.out.println(mirrorEnds("abXYZba"));
-        System.out.println(mirrorEnds(" "));
+        System.out.println(mirrorEnds("abca"));
 
 
     }
     public static String mirrorEnds(String str){
-        String result = " ";
+        String result = "";
 
         for(int i = 0; i < str.length(); i++){
 
@@ -15,7 +15,9 @@ public class Task8 {
             String last = str.substring(str.length() -1 - i, str.length()-i);
 
             if (first.equals(last)){
-               result = first;
+               result = result + first;
+            }else{
+                break;
             }
         }
 
