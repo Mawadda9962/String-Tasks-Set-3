34321aee4b265dba1543008551fdc9f9d09a6b02
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-22 11:11:51 +0400

    Solving task10

--- src/Task9.java
+++ src/Task9.java
@@ -1,17 +1,27 @@
 public class Task9 {
     public static void main(String[] args){
 
-        System.out.println(maxBlock(""));
+        System.out.println(maxBlock("hoopla"));
 
     }
     public static Integer maxBlock(String str){
 
-        int count = 0;
+        int count = 1;
+        int max = 1;
         for(int i = 0; i < str.length(); i++){
             if(str.charAt(i) == str.charAt(i+1)){
                 count++;
+            }else {
+                count = 1;
+
+            }
+
+            if(count > max){
+                return max = count;
             }
         }
-        return count;
+
+
+        return max;
     }
 }
